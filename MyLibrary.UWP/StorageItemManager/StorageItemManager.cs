5f9864dd878e48d7a5096be46b26bd477f6c4182
DJDQfff <2743109998@qq.com>
2023-03-13 20:14:48 +0800

    简单实现一个StorageManager

---
+++ MyLibrary.UWP/StorageItemManager/StorageItemManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Storage;
+
+namespace MyUWPLibrary.StorageItemManager
+{
+    public class StorageItemManager
+    {
+        public ObservableCollection<StorageFolder> Folders = new ObservableCollection<StorageFolder>();
+        public void InitialFolders(IEnumerable<StorageFolder> folders)
+        {
+            Folders.Clear();
+            foreach(var folder in folders)
+            {
+                Folders.Add(folder);
+            }
+        }
+
+        public async Task<StorageFile> GetStorageFile(string filepath)
+        {
+            string folderpath = Path.GetDirectoryName(filepath);
+            string filename= Path.GetFileName(filepath);
+
+            var folder=Folders.SingleOrDefault(f=>f.Path==folderpath);
+            if(folder == null)
+            {
+                return null;
+            }
+            else
+            {
+                var item=await folder.TryGetItemAsync(filename);
+                return item as StorageFile;
+            }
+        }
+
+        
+    }
+}
