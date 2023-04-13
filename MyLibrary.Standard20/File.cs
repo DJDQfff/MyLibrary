c167123523eb779eb126642b8f273200650d5a7c
DJDQfff <2743109998@qq.com>
2023-04-13 10:00:38 +0800

    调整

---
+++ MyLibrary.Standard20/File.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyLibrary.Standard20
+{
+    /// <summary>
+    /// 文件操作封装
+    /// </summary>
+    public static class File
+    {
+        /// <summary>
+        /// 确保存在文件，如果没有则生成一个
+        /// </summary>
+        /// <param name="path"></param>
+        public static void EnsureFileExist(string path)
+        {
+             FileInfo fileInfo = new FileInfo(path);
+            if(!fileInfo.Exists)
+            {
+                fileInfo.Create();
+            }
+        }
+    }
+}
