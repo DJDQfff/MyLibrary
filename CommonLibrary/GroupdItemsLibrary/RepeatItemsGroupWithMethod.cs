1b8b53a3b93fd8be5831ae5a9a6c23fce371b14f
DJDQfff <2743109998@qq.com>
2025-08-12 17:34:12 +0800

    不知道原因的界面卡死

--- CommonLibrary/GroupdItemsLibrary/RepeatItemsGroupWithMethod.cs
+++ CommonLibrary/GroupdItemsLibrary/RepeatItemsGroupWithMethod.cs
@@ -15,33 +15,38 @@ public class RepeatItemsGroupWithMethod<TKey, TElement, TRepeatGroup> : RepeatIt
     public List<TElement> Source { set; get; }
     public event Action<TElement> AddToResult;
 
-    public void StartCompareSequence (IList<TElement> elements , Func<TElement , TElement , TKey> compare , Func<TKey , bool> filt)
+    public async Task StartCompareSequence (IList<TElement> elements , Func<TElement , TElement , TKey> compare , Func<TKey , bool> filt)
     {
         RepeatPairs.Clear();
         while (elements.Count > 1)
         {
             var group = new TRepeatGroup();
 
-            for (int index = elements.Count - 2 ; index >= 0 ; index--)
-            {
-                var key = compare(elements[^1] , elements[index]);
-                if (filt(key))
-                {
-                    if (group.Key is null)
-                    {
-                        group.Initial(key);
-                        group.AddElement(elements[^1]);
+            await Task.Run(() =>
+             {
+                 for (int index = elements.Count - 2 ; index >= 0 ; index--)
+                 {
+                     var key = compare(elements[^1] , elements[index]);
+                     if (filt(key))
+                     {
+                         if (group.Key is null)
+                         {
+                             group.Initial(key);
+                             group.AddElement(elements[^1]);
 
-                    }
-                    if (key.Equals(group.Key))
-                    {
-                        group.AddElement(elements[index]);
-                        elements.Remove(elements[index]);
+                         }
+                         if (key.Equals(group.Key))
+                         {
+                             group.AddElement(elements[index]);
+                             elements.Remove(elements[index]);
 
-                    }
-                }
-            }
-            elements.Remove(elements[^1]);
+                         }
+                     }
+                 }
+                 elements.Remove(elements[^1]);
+
+             });
+
             if (group.Collections.Count >= 2)
             {
                 RepeatPairs.Add(group);
@@ -54,32 +59,49 @@ public class RepeatItemsGroupWithMethod<TKey, TElement, TRepeatGroup> : RepeatIt
         }
     }
 
-    public void ByEachKey (IEnumerable<TElement> elements , Func<TElement , TKey> getkey ,
+    public async Task ByEachKey (IEnumerable<TElement> elements , Func<TElement , TKey> getkey ,
         Func<TRepeatGroup , bool> filt)
     {
         RepeatPairs.Clear();
-        var array = elements.Select(x => getkey(x));
+        var items = new List<TRepeatGroup>();
+        await Task.Run(() =>
+         {
+             //var array = elements.Select(x => getkey(x));
 
-        var a = elements.GroupBy(getkey).SkipWhile(x => x.Key is null);
-        foreach (var cc in a)
+             var a = elements
+             .GroupBy(getkey)
+             .SkipWhile(x => x.Key is null);
+
+             foreach (var cc in a)
+             {
+                 if (cc.Count() > 1)
+                 {
+                     var item = new TRepeatGroup();
+                     item.Initial(cc);
+                     var can = filt?.Invoke(item);
+                     if (can.Value)
+                     {
+                         items.Add(item);
+
+                     }
+
+                 }
+             }
+         });
+
+        foreach (var item in items)
         {
+            RepeatPairs.Add(item);
+            //foreach (var manga in item.Collections)
+            //{
+            //    AddToResult?.Invoke(manga);
+            //}
 
-            if (cc.Count() > 1)
-            {
-                var item = new TRepeatGroup();
-                item.Initial(cc);
-                var can = filt.Invoke(item);
-                if (can)
-                {
-                    RepeatPairs.Add(item);
-                    foreach (var manga in item.Collections)
-                    {
-                        AddToResult?.Invoke(manga);
-                    }
 
-                }
-            }
         }
+
+
+
 
     }
 
